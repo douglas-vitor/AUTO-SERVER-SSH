a86d56a775908e8effbf66ba06abc36ec7f653fb
douglas-vitor <53841865+douglas-vitor@users.noreply.github.com>
2019-09-02 12:29:59 -0300

    Add files via upload

---
+++ servidorSSH_montagem_auto.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+#################
+#Script de servidor ssh basico com montagem automatico
+#v 1.0
+#################
+
+sshverifica=`dpkg -l | cut -b 1-18 | grep openssh-server`
+var1="ii  openssh-server"
+sshdir="/etc/ssh"
+
+if [ "$sshverifica" == "$var1" ] 2> /dev/null; then
+echo "Openssh-server instalado.....[OK]"
+else
+echo "instalando Openssh-server"
+apt-get install openssh-server -y
+fi
+
+while true; do
+read -p 'Parar servidor SSH? [s/n]' OP
+case $OP in
+	n ) echo "continuando Script..." ;;
+	y ) clear; service ssh stop;;
+esac
+echo
+done
+
+service ssh start
+service ssh status
+sleep 5
+
+update-rc.d ssh remove -f &&
+
+update-rc.d -f ssh defaults &&
+
+mkdir $sshdir/inseguras_original_keys_default_kali
+mv $sshdir/ssh_host_* $sshdir/inseguras_original_keys_default_kali &&
+
+dpkg-reconfigure openssh-server &&
+
+cd $sshdir
+mv sshd_config sshd_config_ORIGINAL &&
+
+echo "#	$OpenBSD: sshd_config,v 1.100 2016/08/15 12:32:04 naddy Exp $
+
+# This is the sshd server system-wide configuration file.  See
+# sshd_config(5) for more information.
+
+# This sshd was compiled with PATH=/usr/bin:/bin:/usr/sbin:/sbin
+
+# The strategy used for options in the default sshd_config shipped with
+# OpenSSH is to specify options with their default value where
+# possible, but leave them commented.  Uncommented options override the
+# default value.
+
+#Port 22
+#AddressFamily any
+#ListenAddress 0.0.0.0
+#ListenAddress ::
+
+#HostKey /etc/ssh/ssh_host_rsa_key
+#HostKey /etc/ssh/ssh_host_ecdsa_key
+#HostKey /etc/ssh/ssh_host_ed25519_key
+
+# Ciphers and keying
+#RekeyLimit default none
+
+# Logging
+#SyslogFacility AUTH
+#LogLevel INFO
+
+# Authentication:
+
+#LoginGraceTime 2m
+PermitRootLogin no
+#StrictModes yes
+MaxAuthTries 3
+MaxSessions 5
+
+#PubkeyAuthentication yes
+
+# The default is to check both .ssh/authorized_keys and .ssh/authorized_keys2
+# but this is overridden so installations will only check .ssh/authorized_keys
+AuthorizedKeysFile	.ssh/authorized_keys
+
+#AuthorizedPrincipalsFile none
+
+#AuthorizedKeysCommand none
+#AuthorizedKeysCommandUser nobody
+
+# For this to work you will also need host keys in /etc/ssh/ssh_known_hosts
+#HostbasedAuthentication no
+# Change to yes if you don't trust ~/.ssh/known_hosts for
+# HostbasedAuthentication
+#IgnoreUserKnownHosts no
+# Don't read the user's ~/.rhosts and ~/.shosts files
+#IgnoreRhosts yes
+
+# To disable tunneled clear text passwords, change to no here!
+#PasswordAuthentication yes
+#PermitEmptyPasswords no
+
+# Change to yes to enable challenge-response passwords (beware issues with
+# some PAM modules and threads)
+ChallengeResponseAuthentication no
+
+# Kerberos options
+#KerberosAuthentication no
+#KerberosOrLocalPasswd yes
+#KerberosTicketCleanup yes
+#KerberosGetAFSToken no
+
+# GSSAPI options
+#GSSAPIAuthentication no
+#GSSAPICleanupCredentials yes
+#GSSAPIStrictAcceptorCheck yes
+#GSSAPIKeyExchange no
+
+# Set this to 'yes' to enable PAM authentication, account processing,
+# and session processing. If this is enabled, PAM authentication will
+# be allowed through the ChallengeResponseAuthentication and
+# PasswordAuthentication.  Depending on your PAM configuration,
+# PAM authentication via ChallengeResponseAuthentication may bypass
+# the setting of 'PermitRootLogin without-password'.
+# If you just want the PAM account and session checks to run without
+# PAM authentication, then enable this but set PasswordAuthentication
+# and ChallengeResponseAuthentication to 'no'.
+UsePAM yes
+
+#AllowAgentForwarding yes
+#AllowTcpForwarding yes
+#GatewayPorts no
+X11Forwarding yes
+#X11DisplayOffset 10
+#X11UseLocalhost yes
+#PermitTTY yes
+PrintMotd no
+#PrintLastLog yes
+#TCPKeepAlive yes
+#UseLogin no
+#UsePrivilegeSeparation sandbox
+#PermitUserEnvironment no
+#Compression delayed
+#ClientAliveInterval 0
+#ClientAliveCountMax 3
+#UseDNS no
+#PidFile /var/run/sshd.pid
+#MaxStartups 10:30:100
+#PermitTunnel no
+#ChrootDirectory none
+#VersionAddendum none
+
+# no default banner path
+#Banner none
+
+# Allow client to pass locale environment variables
+AcceptEnv LANG LC_*
+
+# override default of no subsystems
+Subsystem	sftp	/usr/lib/openssh/sftp-server
+
+# Example of overriding settings on a per-user basis
+#Match User anoncvs
+#	X11Forwarding no
+#	AllowTcpForwarding no
+#	PermitTTY no
+#	ForceCommand cvs server " > sshd_config &&
+
+#update-rc.d -f enable 2 3 4 5 &&
+
+service ssh restart &&
+
+echo "Servidor SSH pronto!"
+echo ""
+echo "
+exemplo de como acessa servidor ssh, user@192.168.1.100
+exemplo de como upar arquivos, scp <dir/arquivo> <user>@<ipservidor>:<dir_destino> "
+
+exit 0
